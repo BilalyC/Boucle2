ef4dac92d5a03ec2dc72d53c2f8416457e05903a
BilalyC <bilaly.camara@outlook.com>
2018-12-20 10:46:21 +0100

    Exercice 2 Complet

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 10)// execution de la boucle tant que counter est inférieur ou égal à 10
+            {
+                Console.WriteLine("Bonjour, je suis le message n°" + counter + ".");
+                counter++;//incrémentation de la boucle
+            }
+        }
+    }
+}
